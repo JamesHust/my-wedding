71b39b0709653d441b075c92b60bee97883be5a9
JamesHust <hungjame99@gmail.com>
2022-12-17 15:19:11 +0700

    init project

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// Lib
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Components
+import App from './App.vue'
+import router from './router'
+
+// Plugins
+import {loadFonts} from "@/plugins/webfontloader";
+import vuetify from "@/plugins/vuetify";
+
+// Style
+import "@/assets/common.scss"
+
+loadFonts()
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+// Composable
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+  components,
+  directives,
+})
